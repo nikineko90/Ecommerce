eee86f33ef451f5e7fe67bf10f27418d18ece193
enada996 <77673250+enada996@users.noreply.github.com>
2021-01-21 15:50:17 +0100

    Create progecommerce.sql

---
+++ Mysql/progecommerce.sql
@@ -0,0 +1,81 @@
+create database negozioabbigliamento;
+
+use negozioabbigliamento;
+create table articoli (
+	id int primary key auto_increment,
+    nome varchar(200),
+    codicearticolo varchar(500),
+    categoria varchar(200),
+    prezzo double,
+    colore varchar(200),
+    taglia varchar(20),
+    quantita int,
+    sesso varchar(1),
+    brand varchar(50)
+);
+-- drop table articoli;
+-- vedere se sono uguali sesso e brand
+select * from articoli;
+
+create table utenti (
+	id int primary key auto_increment,
+    username varchar(50),
+    password varchar(50),
+    nome varchar(50),
+    cognome varchar(50),
+    indirizzo varchar(200)
+);
+
+select * from utenti;
+
+-- la tabella acquisti non ce l'ho nel db
+create table acquisti(
+	id int primary key auto_increment,
+	idutente int,
+    idarticolo int,
+    importototale double,
+    foreign key(idutente) references utenti(id),
+    foreign key(idarticolo) references articoli(id)
+);
+-- drop table acquisti;
+
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (1,'Pantaloni professional','1629974374','Pantaloni',14.5,'giallo','M',37,'M','Cosi');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (2,'Reggiseno a balconcino','1951651383','Intimo',65.5,'bianco','M',47,'F','Intimiss');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (3,'Vestito affascinante','3750085870','Vestiti',33.9,'giallo','XS',69,'F','OVC');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (4,'Giacca di pelle','6350243048','Giacche',66.1,'nero','L',5,'F','Guessa');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (5,'T-shirt dinosauro','8791932858','T-shirt e Top',40.4,'blu','XS',92,'M','OVC');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (6,'Pantaloni professional','9467042770','Pantaloni',32.5,'blu','XS',80,'F','Cosi');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (7,'Camicia di seta','7334307070','Camicie',66.8,'rosa','XS',10,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (8,'Camicia di seta','3315159033','Camicie',53.7,'nero','L',62,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (9,'Calze all\'ultima moda','0192022164','Calze',7.1,'verde','M',67,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (10,'Reggiseno a balconcino','7185514541','Intimo',61.3,'nero','XS',91,'F','Intimiss');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (11,'Reggiseno a balconcino','7323328004','Intimo',45.2,'verde','M',68,'F','Intimiss');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (12,'Pantaloni professional','2747659593','Pantaloni',68,'blu','XS',74,'M','Cosi');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (13,'Camicia di seta','6088018828','Camicie',4.8,'rosso','M',95,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (14,'Jeans strappati','8333078362','Jeans',67.2,'giallo','S',98,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (15,'Vestito affascinante','4580693574','Vestiti',31.8,'viola','XL',18,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (16,'Reggiseno a balconcino','8975985911','Intimo',68.1,'giallo','XL',76,'F','Intimiss');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (17,'Giacca di pelle','6126068998','Giacche',18.4,'giallo','L',27,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (18,'Cappotto pesante','2895581649','Cappotti',64.2,'rosso','L',77,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (19,'Giacca di pelle','2719670081','Giacche',71.7,'blu','L',7,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (20,'Cappotto pesante','8638820852','Cappotti',8.6,'nero','S',85,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (21,'Reggiseno a balconcino','2262819890','Intimo',6.1,'rosso','S',52,'F','Intimiss');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (22,'Reggiseno a balconcino','2897487070','Intimo',21.9,'rosso','XL',19,'F','Intimiss');
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (23,'Calze all\'ultima moda','0060227648','Calze',59.1,'rosso','XS',74,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (24,'Vestito affascinante','6333937266','Vestiti',62.3,'rosso','XL',84,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (25,'Jeans strappati','2912102189','Jeans',50.8,'rosso','XS',46,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (26,'Giacca di pelle','9415261811','Giacche',4.6,'viola','M',17,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (27,'Giacca di pelle','5622081459','Giacche',61,'rosa','S',66,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (28,'Jeans strappati','5411959179','Jeans',73.5,'verde','XL',56,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (29,'Camicia di seta','0629477779','Camicie',2.7,'verde','S',45,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (30,'Giacca di pelle','1730389546','Giacche',47.3,'rosa','XS',83,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (31,'Giacca di pelle','7035554327','Giacche',58.6,'rosa','S',49,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (32,'Vestito affascinante','7597331754','Vestiti',75.7,'blu','S',51,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (33,'Cappotto pesante','4731491576','Cappotti',53.8,'viola','XL',65,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (34,'Calze all\'ultima moda','7069005040','Calze',70.5,'verde','XS',7,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (35,'Jeans strappati','2913574777','Jeans',20.4,'giallo','S',13,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (36,'Calze all\'ultima moda','4263144945','Calze',5.6,'giallo','L',75,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (37,'T-shirt dinosauro','1696495776','T-shirt e Top',68.8,'giallo','L',13,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (38,'Vestito affascinante','7696704230','Vestiti',37,'viola','M',7,'M',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (39,'Camicia di seta','9506687102','Camicie',35.2,'giallo','M',56,'F',NULL);
+INSERT INTO `articoli` (`id`,`nome`,`codicearticolo`,`categoria`,`prezzo`,`colore`,`taglia`,`quantita`,`sesso`,`brand`) VALUES (40,'Pantaloni professional','4956558473','Pantaloni',53.5,'verde','XS',75,'M','Cosi');
